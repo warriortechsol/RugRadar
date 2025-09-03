50310899f29fc292183bda9322ef2db50fc6f4e8
warriortechsol <warriortech-solutions@outlook.com>
2025-09-03 14:44:47 -0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig, loadEnv } from 'vite'
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
   return {
+    base: '/RugRadar/', // 👈 matches your repo name
     define: {
-      __API_BASE__: JSON.stringify(env.VITE_API_BASE)
+      __API_BASE__: JSON.stringify(env.VITE_API_BASE || 'https://woreffort.duckdns.org')
+    },
+    css: {
+      postcss: {}
     }
   }
 })
+
 
 
